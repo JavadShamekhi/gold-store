d0ad46867f563c9f8951245d87e5a5cbd2a3665e
JavadShamekhi <javad.shamekhi.80@gmail.com>
2026-06-07 14:55:10 +0330

    app api files

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@/src/lib/prisma';
+
+export async function DELETE(
+		req: Request,
+		{ params }: { params: Promise<{ id: string }> }
+) {
+	const { id } = await params;
+
+	await prisma.product.delete({
+		where: {
+			id: Number(id),
+		},
+	});
+
+	return NextResponse.json({
+		success: true,
+	});
+}
+
+export async function PUT(
+		req: Request,
+		{ params }: { params: Promise<{ id: string }> }
+) {
+	const { id } = await params;
+	const body = await req.json();
+	const product = await prisma.product.update({
+		where: {
+			id: Number(id),
+		},
+		data: {
+			title: body.title,
+			description: body.description,
+			category: body.category,
+			image: body.image,
+			price: body.price,
+			weight: body.weight,
+			stock: body.stock,
+		},
+	});
+
+	return NextResponse.json(product);
+}
---
+++ src/app/api/products/routes.ts
@@ -0,0 +1,27 @@
+import {prisma} from "@/src/lib/prisma";
+import {NextResponse} from "next/server";
+
+export async function POST(req: Request) {
+	try {
+		const body = await req.json();
+
+		const product = await prisma.product.create({
+			data: {
+				title: body.title,
+				description: body.description,
+				price: body.price,
+				weight: body.weight,
+				stock: body.stock,
+				image: body.image,
+				category: body.category,
+			},
+		});
+
+		return NextResponse.json(product);
+	} catch {
+		return NextResponse.json(
+				{ error: 'Failed to create product' },
+				{ status: 500 }
+		);
+	}
+}
