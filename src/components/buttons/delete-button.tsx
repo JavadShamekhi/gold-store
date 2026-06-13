937dee8adb82efb197d69a8f3f571906aaf825cf
JavadShamekhi <javad.shamekhi.80@gmail.com>
2026-06-13 18:44:37 +0330

    edit components

--- src/components/buttons/delete-button.tsx
+++ src/components/buttons/delete-button.tsx
@@ -13,7 +13,6 @@ import {
 	AlertDialogTitle,
 	AlertDialogTrigger,
 } from '@/src/components/ui/alert-dialog';
-import { Button } from '@/src/components/ui/button';
 
 export default function DeleteButton({
 	                                     id,
