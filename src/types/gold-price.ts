f45bec67505b6e6ad5300cc2c7ec21c33be49f04
JavadShamekhi <javad.shamekhi.80@gmail.com>
2026-06-16 12:04:07 +0330

    create gold price type

---
+++ src/types/gold-price.ts
@@ -0,0 +1,12 @@
+export type GoldPriceItem = {
+	date: string;
+	time: string;
+	time_unix: number;
+	symbol: string;
+	name_en: string;
+	name: string;
+	price: number;
+	change_value: number;
+	change_percent: number;
+	unit: string;
+};
